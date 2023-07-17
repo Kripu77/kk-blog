8e083c814428a8e032974a02204081430425e910
Kripu Khadka <84505567+Kripu77@users.noreply.github.com>
2023-07-17 20:33:17 +1000

    blogs shell

---
+++ app/(blogs)/blogs/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Blogs",
+    description:
+        "Welcome to my blog. Here I try to reiterate my learning as I craft the art of Software Engineering with me. Come let's learn together.",
+};
+
+const BlogsPage = () => {
+    return <div>page</div>;
+};
+
+export default BlogsPage;
