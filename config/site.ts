c44c0b16d984b9e85d3e41d777ad316055b6a8f0
Kripu Khadka <khadkakripu4@gmail.com>
2024-09-01 12:14:01 +1000

    add footer metadata

--- config/site.ts
+++ config/site.ts
@@ -3,6 +3,12 @@ import { SiteConfig } from "@/types";
 export const siteConfig: SiteConfig = {
   title: "kripukhadka ✨",
   description: "Hi, I'm Kripu Khadka. I'm a full-stack developer.",
+  authors: [
+    {
+      name: "Kripu Khadka",
+      url: "https://kripukhadka.com",
+    },
+  ],
   navItems: [
     {
       title: "Home",
@@ -11,10 +17,37 @@ export const siteConfig: SiteConfig = {
       description: "Home page",
     },
     {
-      title: "blogs",
+      title: "Blogs",
       href: "/blogs",
       label: "Blogs",
       description: "Blogs page",
     },
   ],
+  footerItems: {
+    title: "&copy; Kripu Khadka ✨",
+    items: [
+      {
+        title: "Github",
+        url: "https://github.com/Kripu77",
+        description: "Github",
+        icon: "github",
+      },
+      {
+        title: "LinkedIn",
+        url: "https://www.linkedin.com/in/kripu-khadka/",
+        description: "LinkedIn",
+        icon: "linkedin",
+      },
+      {
+        title: "Mail",
+        url: "mailto:khadkakripu4@gmail.com",
+        description: "Mail",
+        icon: "mail",
+      },
+    ],
+    footerCopyRightInfo: {
+      year: new Date().getFullYear(),
+      owner: "Kripu Khadka",
+    },
+  },
 };
