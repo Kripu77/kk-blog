9c642c35dbee9ad768f235b27668139ca1954272
Kripu Khadka <khadkakripu4@gmail.com>
2024-09-02 20:27:00 +1000

    framer animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,31 @@
+"use client";
+
 import { AboutSection } from "@/components/about-section";
-import KripuKhadka from "@/assets/kripukhadka.webp"
+import KripuKhadka from "@/assets/kripukhadka.webp";
 import AboutCard from "@/components/about-card";
+
+import { motion } from "framer-motion";
+
+
 
 
 export default function Home() {
-    return (
-        <main>
-         <AboutSection ImageLink={KripuKhadka} title="Hi, 👋 I'm Kripu Khadka!" description="I am a Software Engineer who specializes in designing and developing platforms to solve problems through code."/>
-         <AboutCard/>
-        </main>
-    );
+  return (
+    <main>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        exit={{ opacity: 0, transition: { duration: 5} }}
+        variants={{ visible: { transition: { staggerChildren: 0.4} } }}
+      >
+        <AboutSection
+          ImageLink={KripuKhadka}
+          title="Hi, 👋 I'm Kripu Khadka!"
+          description="Software Engineer specializing in crafting innovative solutions through code...💭"
+        />
+
+        <AboutCard />
+      </motion.div>
+    </main>
+  );
 }
